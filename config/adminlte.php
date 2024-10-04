af25b74a9aa3d82a50c4694846b6df299afb0ab2
PabloProgramador77 <pabloprogramador77@gmail.com>
2024-10-04 12:39:42 -0600

    Ajustes de Branding

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'C.A.R.N.E.',
+    'title' => 'La Higienica Premium',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,8 +63,8 @@ return [
     |
     */
 
-    'logo' => '<b>C.A.R.N.E.</b>',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '<b>La Higienica Premium</b>',
+    'logo_img' => 'img/logo-min-removebg-preview.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
@@ -86,11 +86,11 @@ return [
     'auth_logo' => [
         'enabled' => false,
         'img' => [
-            'path' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+            'path' => 'img/logo-min-removebg-preview.png',
             'alt' => 'Auth Logo',
             'class' => '',
             'width' => 50,
-            'height' => 50,
+            'height' => 80,
         ],
     ],
 
@@ -113,11 +113,11 @@ return [
         'enabled' => true,
         'mode' => 'fullscreen',
         'img' => [
-            'path' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+            'path' => 'img/logo-min-removebg-preview.png',
             'alt' => 'AdminLTE Preloader Image',
             'effect' => 'animation__shake',
-            'width' => 120,
-            'height' => 120,
+            'width' => 360,
+            'height' => 360,
         ],
     ],
 
