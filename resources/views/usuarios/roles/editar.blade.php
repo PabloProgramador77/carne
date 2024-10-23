6d3ccd9aa10d0c339e3e93a18cd7e34a61a54b35
PabloProgramador77 <pabloprogramador77@gmail.com>
2024-10-23 14:22:00 -0600

    crud base de roles de usuario

---
+++ resources/views/usuarios/roles/editar.blade.php
@@ -0,0 +1,27 @@
+<x-adminlte-modal id="modalEditar" size="lg" title="Editar Rol" theme="primary" icon="fas fa-edit" static-backdrop scrollable>
+    <div class="container-fluid row">
+        <div class="col-lg-5">
+            <img src="{{ asset('img/cliente.jpg') }}" alt="" width="100%" height="auto" class="shadow">
+        </div>
+        <div class="col-lg-7">
+            <form novalidate>
+                <small class="fs-6 fw-semibold bg-warning border-bottom p-1 d-block mb-2">Cambia los datos como creas necesario. Los campos con * son obligatorios</small>
+                <div class="form-group">
+                    <x-adminlte-input name="nombreEditar" id="nombreEditar" placeholder="Rol de usuario">
+                        <x-slot name="prependSlot">
+                            <div class="input-group-text text-info">
+                                <i class="fas fa-user">*</i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
+                </div>
+                <input type="hidden" name="id" id="id">
+            </form>
+        </div>
+        
+    </div>
+    <x-slot name="footerSlot">
+        <x-adminlte-button theme="success" label=" Actualizar" id="actualizar" icon="fas fa-save"></x-adminlte-button>
+        <x-adminlte-button theme="danger" label=" Cancelar" id="cancelar" data-dismiss="modal" icon="fas fa-window-close"></x-adminlte-button>
+    </x-slot>
+</x-adminlte-modal>
---
+++ resources/views/usuarios/roles/index.blade.php
@@ -0,0 +1,72 @@
+@extends('home')
+@section('contenido')
+
+    <section class="container-fluid p-2 my-2 bg-white rounded shadow">
+
+        <div class="container-fluid row border-bottom">
+
+            <div class="col-lg-5">
+                <h1 class="fs-3 fw-semibold"><i class="fas fa-user-tag"></i> Mis Roles</h1>
+                <p class="fs-6 fw-semibold text-secondary"><i class="fas fa-user-shield"></i> Panel de Administrador</p>
+            </div>
+            
+            <div class="col-lg-5 my-2">
+                <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/home"><i class="fas fa-home"></i> Inicio</a></li>
+                        <li class="breadcrumb-item active" aria-current="page"><i class="fas fa-user-tag"></i> Roles de usuario</li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div class="col-lg-2 my-2">
+                <x-adminlte-button theme="primary" data-toggle="modal" data-target="#modalNuevo" icon="fas fa-plus-circle" title="Nuevo rol" label=""></x-adminlte-button>
+            </div>
+
+            <div class="col-lg-12">
+                <small class="p-1 bg-warning text-center rounded d-block">Agrega nuevos roles de rol con el botón <i class="fas fa-plus-circle"></i> o administra los existentes con los botones correspondientes.</small>
+            </div>
+
+        </div>
+
+        <div class="container-fluid row p-2">
+            
+            @if( count( $roles ) > 0 )
+                @php
+                    $heads = ['Folio', 'Nombre', 'Acciones', ''];
+                @endphp
+
+                <x-adminlte-datatable id="contenedorroles" theme="light" :heads="$heads" striped hoverable compressed beautify>
+                    @foreach( $roles as $rol )
+                        <tr>
+                            <td>{{ $rol->id }}</td>
+                            <td>{{ $rol->name }}</td>
+                            <td>
+                                <x-adminlte-button class="shadow editar" icon="fas fa-edit" theme="info" data-toggle="modal" data-target="#modalEditar" data-id="{{ $rol->id }}" data-value="{{ $rol->name }}"></x-adminlte-button>
+                                <x-adminlte-button class="shadow borrar" icon="fas fa-trash" theme="danger" data-id="{{ $rol->id }}" data-value="{{ $rol->name }}"></x-adminlte-button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </x-adminlte-datatable>
+                
+            @else
+                <tr>
+                    <td colspan="5" class="text-danger"><i class="fas fa-info-circle"></i> Sin roles registrados.</td>
+                </tr>
+            @endif
+
+        </div>
+
+    </section>
+
+    @include('usuarios.roles.nuevo')
+    @include('usuarios.roles.editar')
+
+    <script src="{{ asset('js/jquery-3.7.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/sweetAlert.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/roles/create.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/roles/read.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/roles/update.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/roles/delete.js') }}" type="text/javascript"></script>
+
+@stop
